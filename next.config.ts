e0d83ed790e5d4dbd2553f88e76840ed89be55bc
abaoxomtieu <htbqn2003@gmail.com>
2025-06-19 14:37:15 +0700

    Add Google user content domains to Next.js configuration for image optimization

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+    domains: ["lh3.googleusercontent.com", "*.googleusercontent.com"],
   },
 };
 const withNextIntl = createNextIntlPlugin();
