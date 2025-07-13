370ff97f2fb3a59a10a3fdf1067f9f7d164a97e3
abaoxomtieu <htbqn2003@gmail.com>
2025-07-13 15:11:45 +0700

    refactor: streamline image configuration in next.config.ts for better performance
    - Consolidate remote image patterns for Google user content to enhance image loading efficiency.
    - Update domains for image sources to improve compatibility and security.
    - Remove unnecessary device and image size configurations for a cleaner setup.

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,24 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-
 const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "lh4.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh5.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh6.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ssl.gstatic.com",
+        hostname: "*.googleusercontent.com",
+        port: "",
         pathname: "/**",
       },
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ["image/webp", "image/avif"],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ["lh3.googleusercontent.com", "*.googleusercontent.com"],
   },
 };
-
 const withNextIntl = createNextIntlPlugin();
-
 export default withNextIntl(nextConfig);
